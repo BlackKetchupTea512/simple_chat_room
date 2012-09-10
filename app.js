75de44e818e1b3b37ee0dcd32af2e946e442417b
blue_ham_cake <bluehamcake@gmail.com>
2012-09-10 20:29:09 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
+var app = express()
+  , server = http.createServer(app)
+  , io = require('socket.io').listen(server);
+
+server.listen(3000);
+
+io.sockets.on('connection', function (socket) {
+  io.sockets.emit("send message", "server","誰か来ました。");
+
+  socket.on('send message', function (name, message) {
+    console.log(name, message);
+    socket.broadcast.emit("send message", name, message);
+  });
+});
+
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
